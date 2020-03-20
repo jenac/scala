aaed5388699945b2c8cc249fce0faf0b3d8e9fe9
Lihe Chen <lihe.chen@gmail.com>
2020-03-19 21:37:50 -0500

    refactor

---
+++ scala-function-programming/src/main/scala/StrictnessAndLaziness.scala
@@ -0,0 +1,132 @@
+object StrictnessAndLaziness {
+
+  //5.1
+  def toList[A](s: LazyList[A]): List[A] = s match { //Stream is obsolete
+    case h #:: t => h :: toList(t)
+    case _ => List()
+  }
+
+  //5.2
+  def take[A](s: LazyList[A], n: Int): LazyList[A] = s match {
+    case h #:: t if n > 0 => h #:: t.take(n - 1)
+    case h #:: _ if n == 0 => h #:: LazyList.empty
+    case _ => LazyList.empty
+  }
+
+  def drop[A](s: LazyList[A], n: Int): LazyList[A] = s match {
+    case _ #:: t if n > 0 => t.drop(n - 1)
+    case _ => s
+  }
+
+  //5.3
+  def takeWhile[A](input: LazyList[A])(p: A => Boolean): LazyList[A] = input match {
+    case h #:: t if p(h) => h #:: takeWhile(t)(p)
+    case h #:: t => takeWhile(t)(p)
+    case _ => LazyList.empty
+  }
+
+  //5.4
+  def forAll[A](input: LazyList[A])(p: A => Boolean): Boolean = input match {
+    case h #:: t if p(h) => forAll(t)(p)
+    case _ #:: _ => false
+    case _ => true
+  }
+
+  def forAll2[A](s: LazyList[A], f: A => Boolean): Boolean =
+    s.foldRight(true)((a, b) => f(a) && b)
+
+  //5.5
+  def takeWhileViaFoldRight[A](input: LazyList[A])(p: A => Boolean): LazyList[A] =
+    input.foldRight(LazyList.empty[A])((a, b) =>
+      if (p(a)) a #:: b
+      else LazyList.empty)
+
+  //5.6
+  def headOption[A](input: LazyList[A]): Option[A] =
+    input.foldRight(None: Option[A])((h, _) => Some(h))
+
+  //5.7
+  def map[A, B](input: LazyList[A])(f: A => B): LazyList[B] =
+    input.foldRight(LazyList.empty[B])((e, acc) => f(e) #:: acc)
+
+  def filter[A](input: LazyList[A])(f: A => Boolean): LazyList[A] =
+    input.foldRight(LazyList.empty[A])((e, acc) => if (f(e)) e #:: acc else acc)
+
+  def append[A, B >: A](a: LazyList[A], b: LazyList[B]): LazyList[B] =
+    a.foldRight(b)((e, acc) => e #:: acc)
+
+  def flatMap[A, B](input: LazyList[A])(f: A => LazyList[B]): LazyList[B] =
+    input.foldRight(LazyList.empty[B])((h, t) => append(f(h), t))
+
+  //5.8
+  def constant[A](a: A): LazyList[A] = a #:: constant(a)
+
+  //5.9
+  def from(n: Int): LazyList[Int] = n #:: from(n + 1)
+
+  //5.10
+  val fibs = {
+    def go(f0: Int, f1: Int): LazyList[Int] = f0 #:: go(f1, f0 + f1)
+
+    go(0, 1)
+  }
+
+  //5.11
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): LazyList[A] = f(z) match {
+    case Some((a, s)) => a #:: unfold(s)(f)
+    case None => LazyList.empty[A]
+  }
+
+  //5.12
+  def fibsViaUnfold =
+    unfold((0, 1)) { case (f0, f1) => Some((f0, (f1, f0 + f1))) }
+
+  def fromViaUnfold(n: Int) = unfold(n)(n => Some((n, n + 1)))
+
+  def constantsViaUnfold(n: Int) = unfold(n)(n => Some(n, n))
+
+  def onesViaUnfold = unfold(1)(_ => Some((1, 1)))
+
+  //5.13
+  def mapViaUnfold[A, B](i: LazyList[A])(f: A => B): LazyList[B] =
+    unfold(i) {
+      _ match {
+        case (h #:: t) => Some((f(h), t))
+        case _ => None
+      }
+    }
+
+  def takeViaUnfold[A](input: LazyList[A], n: Int): LazyList[A] =
+    unfold(input, n) {
+      _ match {
+        case (h #:: t, 1) => Some((h, (t, 0)))
+        case (h #:: t, n) if n > 1 => Some((h, (t, n - 1)))
+        case _ => None
+      }
+    }
+
+  def takeWhileViaUnfold[A](input: LazyList[A])(f: A => Boolean): LazyList[A] =
+    unfold(input) {
+      _ match {
+        case h #:: t if (f(h)) => Some(h, t)
+        case _ => None
+      }
+    }
+
+  def zipWith[A, B, C](s1: LazyList[A], s2: LazyList[B])(f: (A, B) => C): LazyList[C] =
+    unfold((s1, s2)) {
+      case (h1 #:: t1, h2 #:: t2) => Some(f(h1, h2), (t1, t2))
+      case _ => None
+    }
+
+  def zipAll[A, B](s1: LazyList[A], s2: LazyList[B]): LazyList[(Option[A], Option[B])] = zipWithAll(s1, s2)((_, _))
+
+  def zipWithAll[A, B, C](s1: LazyList[A], s2: LazyList[B])(f: (Option[A], Option[B]) => C): LazyList[C] =
+    unfold((s1, s2)) {
+      case (LazyList(), LazyList()) => None
+      case (h #:: t, LazyList()) => Some(f(Some(h), None) -> (t, LazyList()))
+      case (LazyList(), h #:: t) => Some(f(None, Some(h)) -> (LazyList() -> t))
+      case (h1 #:: t1, h2 #:: t2) => Some(f(Some(h1), Some(h2)) -> (t1 -> t2))
+    }
+
+}
--- scala-function-programming/src/test/scala/StrictnessAndLazinessSpec.scala
+++ scala-function-programming/src/test/scala/StrictnessAndLazinessSpec.scala
@@ -2,208 +2,82 @@ import java.time.LocalDate
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
+import StrictnessAndLaziness._
 
 class StrictnessAndLazinessSpec extends AnyFlatSpec with Matchers {
 
   "5.1" should "implement toList" in {
-    def toList[A](s: LazyList[A]): List[A] = s match { //Stream is obsoleted
-      case h #:: t => h :: toList(t)
-      case _ => List()
-    }
-
     val s = LazyList(1, 2, 3)
     toList(s) shouldBe List(1, 2, 3)
   }
 
   "5.2" should "implement take and drop" in {
-    def take[A](s: LazyList[A], n: Int): LazyList[A] = s match {
-      case h #:: t if n > 0 => h #:: t.take(n - 1)
-      case h #:: _ if n == 0 => h #:: LazyList.empty
-      case _ => LazyList.empty
-    }
-
     take(LazyList(1, 2, 3), 2).toList shouldBe List(1, 2)
-
-    def drop[A](s: LazyList[A], n: Int): LazyList[A] = s match {
-      case _ #:: t if n > 0 => t.drop(n - 1)
-      case _ => s
-    }
-
     drop(LazyList(1, 2, 3, 4, 5), 2).toList shouldBe List(3, 4, 5)
   }
 
   "5.3" should "implement takeWhile" in {
-    def takeWhile[A](input: LazyList[A])(p: A => Boolean): LazyList[A] = input match {
-      case h #:: t if p(h) => h #:: takeWhile(t)(p)
-      case h #:: t => takeWhile(t)(p)
-      case _ => LazyList.empty
-    }
-
     takeWhile(LazyList(1, 2, 3))(_ % 2 == 0) shouldBe LazyList(2)
     takeWhile(LazyList(1, 2, 3, 4, 5))(_ < 3).toList shouldBe LazyList(1, 2)
     takeWhile(LazyList(1, 2, 3, 4, 5))(_ < 0).toList shouldBe LazyList.empty
   }
 
   "5.4" should "implement forAll" in {
-    def forAll[A](input: LazyList[A])(p: A => Boolean): Boolean = input match {
-      case h #:: t if p(h) => forAll(t)(p)
-      case _ #:: _ => false
-      case _ => true
-    }
-
     forAll(LazyList(1, 2, 3))(_ % 2 == 0) shouldBe false
     forAll(LazyList("a", "b", "c"))(_.size > 0) shouldBe true
-
-    def forAll2[A](s: LazyList[A], f: A => Boolean): Boolean =
-      s.foldRight(true)((a, b) => f(a) && b)
 
     forAll2(LazyList(1, 2, 3), (x: Int) => x % 2 == 0) shouldBe false
     forAll2(LazyList("a", "b", "c"), (x: String) => x.size > 0) shouldBe true
   }
 
   "5.5" should "implement takeWhile using foldRight" in {
-    def takeWhile[A](input: LazyList[A])(p: A => Boolean): LazyList[A] =
-      input.foldRight(LazyList.empty[A])((a, b) =>
-        if (p(a)) a #:: b
-        else LazyList.empty)
-
-    takeWhile(LazyList(1, 2, 3, 4, 5))(_ < 3).toList shouldBe LazyList(1, 2)
-    takeWhile(LazyList(1, 2, 3, 4, 5))(_ < 0).toList shouldBe LazyList.empty
+    takeWhileViaFoldRight(LazyList(1, 2, 3, 4, 5))(_ < 3).toList shouldBe LazyList(1, 2)
+    takeWhileViaFoldRight(LazyList(1, 2, 3, 4, 5))(_ < 0).toList shouldBe LazyList.empty
   }
 
   "5.6" should "implement headOption with foldRight" in {
-    def headOption[A](input: LazyList[A]): Option[A] =
-      input.foldRight(None: Option[A])((h, _) => Some(h))
-
     headOption(LazyList(1, 2, 3)) shouldBe Some(1)
     headOption(LazyList.empty) shouldBe None
   }
 
   "5.7" should "implement map, filter, append, flatMap using foldRight" in {
-    def map[A, B](input: LazyList[A])(f: A => B): LazyList[B] =
-      input.foldRight(LazyList.empty[B])((e, acc) => f(e) #:: acc)
-
     map(LazyList(1, 2, 3))(_ * 2) shouldBe LazyList(2, 4, 6)
-
-    def filter[A](input: LazyList[A])(f: A => Boolean): LazyList[A] =
-      input.foldRight(LazyList.empty[A])((e, acc) => if (f(e)) e #:: acc else acc)
-
     filter(LazyList(1, 2, 3, 4, 5, 6))(_ <= 3) shouldBe LazyList(1, 2, 3)
-
-    def append[A, B >: A](a: LazyList[A], b: LazyList[B]): LazyList[B] =
-      a.foldRight(b)((e, acc) => e #:: acc)
-
     append(LazyList("a", "b", "c"), LazyList("1", "2", "3")) shouldBe LazyList("a", "b", "c", "1", "2", "3")
-
-    def flatMap[A, B](input: LazyList[A])(f: A => LazyList[B]): LazyList[B] =
-      input.foldRight(LazyList.empty[B])((h, t) => append(f(h), t))
-
     flatMap(LazyList(2, 3))(s => (0 to s).to(LazyList)) shouldBe LazyList(0, 1, 2, 0, 1, 2, 3)
   }
 
   "5.8" should "define infinity constants" in {
-    def constant[A](a: A): LazyList[A] = a #:: constant(a)
-
     constant(12).take(3) shouldBe LazyList(12, 12, 12)
     constant(12).take(5) shouldBe LazyList(12, 12, 12, 12, 12) //also right
   }
 
   "5.9" should "implement infinite from n" in {
-    def from(n: Int): LazyList[Int] = n #:: from(n + 1)
-
     from(6).take(3) shouldBe LazyList(6, 7, 8)
   }
 
   "5.10" should "implement infinite fib" in {
-    val fibs = {
-      def go(f0: Int, f1: Int): LazyList[Int] = f0 #:: go(f1, f0 + f1)
-
-      go(0, 1)
-    }
-
     fibs.take(3) shouldBe LazyList(0, 1, 1)
     fibs.take(7) shouldBe LazyList(0, 1, 1, 2, 3, 5, 8)
   }
 
-  "5.11" should "implement unfold" in {
-    def unfold[A, S](z: S)(f: S => Option[(A, S)]): LazyList[A] = f(z) match {
-      case Some((a, s)) => a #:: unfold(s)(f)
-      case None => LazyList.empty[A]
-    }
-
-    //5.12 implement fibs, from, constants and ones
-    def fibsViaUnfold =
-      unfold((0, 1)) { case (f0, f1) => Some((f0, (f1, f0 + f1))) }
-
+  "5.11/12/13" should "implement unfold, implement fibs, from, constants and ones, implement map, take, takeWhile, zipWith using unfold" in {
     fibsViaUnfold.take(7).toList shouldBe List(0, 1, 1, 2, 3, 5, 8)
-
-    def fromViaUnfold(n: Int) = unfold(n)(n => Some((n, n + 1)))
-
     fromViaUnfold(100).take(5) shouldBe LazyList(100, 101, 102, 103, 104)
-
-    def constantsViaUnfold(n: Int) = unfold(n)(n => Some(n, n))
-
     constantsViaUnfold(12).take(3) shouldBe LazyList(12, 12, 12)
-
-    def onesViaUnfold = unfold(1)(_ => Some((1, 1)))
-
     onesViaUnfold.take(5) shouldBe LazyList(1, 1, 1, 1, 1)
 
-    //5.13 implement map, take, takeWhile, zipWith using unfold
-    def mapViaUnfold[A, B](i: LazyList[A])(f: A => B): LazyList[B] =
-      unfold(i) {
-        _ match {
-          case (h #:: t) => Some((f(h), t))
-          case _ => None
-        }
-      }
-
     mapViaUnfold(LazyList(1, 2, 3, 4))(_.toString) shouldBe LazyList("1", "2", "3", "4")
-
-    def takeViaUnfold[A](input: LazyList[A], n: Int): LazyList[A] =
-      unfold(input, n) {
-        _ match {
-          case (h #:: t, 1) => Some((h, (t, 0)))
-          case (h #:: t, n) if n > 1 => Some((h, (t, n - 1)))
-          case _ => None
-        }
-      }
 
     takeViaUnfold(LazyList(1, 2, 3, 4), 2) shouldBe LazyList(1, 2)
     takeViaUnfold(LazyList(1, 2, 3, 4), 0) shouldBe LazyList.empty[Int]
     takeViaUnfold(LazyList(1, 2, 3, 4), 8) shouldBe LazyList(1, 2, 3, 4)
     takeViaUnfold(LazyList.empty[Int], 8) shouldBe LazyList.empty[Int]
 
-    def takeWhileViaUnfold[A](input: LazyList[A])(f: A => Boolean): LazyList[A] =
-      unfold(input) {
-        _ match {
-          case h #:: t if (f(h)) => Some(h, t)
-          case _ => None
-        }
-      }
-
     takeWhileViaUnfold(LazyList(1, 2, 3, 4))(_ < 8) shouldBe LazyList(1, 2, 3, 4)
     takeWhileViaUnfold(LazyList(1, 2, 3, 4))(_ < 3) shouldBe LazyList(1, 2)
 
-    def zipWith[A, B, C](s1: LazyList[A], s2: LazyList[B])(f: (A, B) => C): LazyList[C] =
-      unfold((s1, s2)) {
-        case (h1 #:: t1, h2 #:: t2) => Some(f(h1, h2), (t1, t2))
-        case _ => None
-      }
-
-
     zipWith(LazyList(2, 2, 2), LazyList(1, 2, 3))(_ + _) shouldBe LazyList(3, 4, 5)
-
-
-    def zipAll[A, B](s1: LazyList[A], s2: LazyList[B]): LazyList[(Option[A], Option[B])] = zipWithAll(s1, s2)((_, _))
-
-    def zipWithAll[A, B, C](s1: LazyList[A], s2: LazyList[B])(f: (Option[A], Option[B]) => C): LazyList[C] =
-      unfold((s1, s2)) {
-        case (LazyList(), LazyList()) => None
-        case (h #:: t, LazyList()) => Some(f(Some(h), None) -> (t, LazyList()))
-        case (LazyList(), h #:: t) => Some(f(None, Some(h)) -> (LazyList() -> t))
-        case (h1 #:: t1, h2 #:: t2) => Some(f(Some(h1), Some(h2)) -> (t1 -> t2))
-      }
 
     zipAll(LazyList(1, 2, 3), LazyList("a", "b", "c")) shouldBe LazyList((Some(1), Some("a")), (Some(2), Some("b")), (Some(3), Some("c")))
     zipAll(LazyList(1, 2), LazyList("a", "b", "c")) shouldBe LazyList((Some(1), Some("a")), (Some(2), Some("b")), (None, Some("c")))
